cea8e0f01ab0d7226a1469c84e441fe1aee45cea
YaroslavYaryk <duhanov2003@gmail.com>
2022-12-02 07:25:20 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Products from "./admin/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./main/Main";
+import ProductsCreate from "./admin/ProductCreate";
+import ProductsEdit from "./admin/ProductEdit";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/products/create" element={<ProductsCreate />} />
+          <Route path="/admin/products/:id/edit" element={<ProductsEdit />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
